7c6d51bf5ff1f5296eb493351ba9846c43e7c0ca
Sakshi <jaiswalsakshi7890@gmail.com>
2024-03-10 14:14:57 +0530

    login and body

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 
 import './App.css';
-
+import Body from './components/Body';
 function App() {
   return (
-    <div className="text-3xl font-bold text-green-800">
-    
-       
-        <p>
-          hello from react, let's make netflixgpt.
-        </p>
-        
-       </div>
+    <>
+    <Body/>
+    </>
   );
 }
 
