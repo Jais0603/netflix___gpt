af7fc7a75e3217533e6002c8ddc05085146dabf4
Sakshi <jaiswalsakshi7890@gmail.com>
2024-03-10 13:59:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hello from react, let's make netflixgpt.
         </p>
         <a
           className="App-link"
