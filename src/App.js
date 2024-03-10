927edf4be36918c326085835d28f56b4cd046dcd
Sakshi <jaiswalsakshi7890@gmail.com>
2024-03-10 14:09:16 +0530

    update readme

--- src/App.js
+++ src/App.js
@@ -3,22 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="text-3xl font-bold text-green-800">
+    
        
         <p>
           hello from react, let's make netflixgpt.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        
+       </div>
   );
 }
 
